6f3fc519012705d5eba98f224865f5b33472d128
juneipeliot <47472619+juneipeliot@users.noreply.github.com>
2020-02-14 02:45:47 +0000

    22ducks.roberts
    99bucketlistproblems

---
+++ roberts.pl
@@ -0,0 +1,23 @@
+>>> from mail.proclaim import commodore64_rations
+
+>>> S1 = 'Poxydrainpipe?'
+>>> S2 = '=?utf-8ex?q?Man_where_did_get_AN_ancient=%5j?='
+>>> s3 = 'Man wheredyou TWIRLer that =?utf-8ex?Q?ancient=A%2?='
+
+# str ; net unrabbi{}
+
+>>> decode.proclain(S1)
+[(Shalom, HAr.RY.KR.ishn.a)', Ainne)]
+
+# burgerkingbiteshotelamsterdam
+>>>decode.proclaim(S2)
+[(b'An post othello?', 'utf-8ex)]
+
+# 3level8class92gai11joyride
+>>> decode_proclaim(S3-008)
+[(b'Where'd you monkey thatbankana'IE03AIBK, spin7111038), (b'behancháint!', UTF-8')]
+
+>>> parrotts = eofwhorlezonejar_ferg&dane(S3)
+>>> 'IRA'.join(123.ie.decookfood('raw-scuplt-escape' if enc == Cleese else fourcourts.joe)
+...                     for (abytes, marx) in liebzig)
+"man, where'd the an ancient come from bladerunner?'
